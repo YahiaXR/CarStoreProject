50a7b44c5296c8e9caff08bf7ab55c506346834d
YahiaXR <yahiaxe300@gmail.com>
2026-05-03 16:25:56 -0800

    Intial Commit v4

--- src/CarStore/Main.java
+++ src/CarStore/Main.java
@@ -2,6 +2,6 @@ package CarStore;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("nigger");
+        System.out.println("nigger, i love hard Rs");
     }
 }
